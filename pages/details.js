70d5d2d371257dc1cd18247505c823e6554282b5
Alejandra <alejandrabarros.tds1.senai@gmail.com>
2025-03-31 22:47:21 -0300

    refactor: adjust text margin

--- pages/details.js
+++ pages/details.js
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#F0FFF0',
     },
     title: {
+        marginBottom: 20,
         fontSize: 24,
         fontWeight: 'bold',
         color: '#9ACD32',
